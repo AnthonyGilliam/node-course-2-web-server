d7191a04a1cb3a878a17ee8e592428c689ea2651
Anthony <anthony.gilliam@gmail.com>
2018-05-01 13:25:28 -0400

    Adding Projects page to site

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
         });
 });
 
+app.get('/projects', (req, res) => {
+    res.render("projects.hbs",
+        {
+            pageTitle: 'GitHub Portfolio',
+        });
+});
+
 app.get('/bad', (req, res) => {
     res.send(
         {
